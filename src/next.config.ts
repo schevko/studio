01c7ad4f685b6b12f0cc524af7c14ece898d88f1
Şevket Erer <zentamedia55@gmail.com>
2025-05-14 10:40:23 +0000

    Projeyi deploy ederken hata alıyorum:
    Running build in Washington, D.C., USA (East) – iad1
    Build machine configuration: 2 cores, 8 GB
    Cloning github.com/schevko/assisto-website (Branch: master, Commit: 5a5f5c4)
    Skipping build cache, deployment was triggered without cache.
    Cloning completed: 505.000ms
    Running "vercel build"
    Vercel CLI 41.7.3
    Installing dependencies...
    npm warn deprecated rimraf@2.7.1: Rimraf versions prior to v4 are no longer supported
    npm warn deprecated inflight@1.0.6: This module is not supported, and leaks memory. Do not use it. Check out lru-cache if you want a good and tested way to coalesce async requests by a key value, which is much more comprehensive and powerful.
    npm warn deprecated @types/handlebars@4.1.0: This is a stub types definition. handlebars provides its own type definitions, so you do not need this installed.
    npm warn deprecated glob@7.2.3: Glob versions prior to v9 are no longer supported
    added 738 packages in 24s
    98 packages are looking for funding
      run `npm fund` for details
    Detected Next.js version: 15.2.3
    Running "npm run build"
    > nextn@0.1.0 build
    > next build
    Attention: Next.js now collects completely anonymous telemetry regarding usage.
    This information is used to shape Next.js' roadmap and prioritize features.
    You can learn more, including how to opt-out if you'd not like to participate in this anonymous program, by visiting the following URL:
    https://nextjs.org/telemetry

---
+++ src/next.config.ts
@@ -0,0 +1,29 @@
+import type {NextConfig} from 'next';
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: false,
+  },
+  eslint: {
+    ignoreDuringBuilds: false,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      }
+    ],
+  },
+};
+
+export default nextConfig;
